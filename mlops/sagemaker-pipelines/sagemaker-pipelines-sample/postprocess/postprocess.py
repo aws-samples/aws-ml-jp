837534fd0425b618679900c880808de124f48235
Kazuhito Go <kazuhitogo@gmail.com>
2022-10-17 09:55:39 +0900

    Update postprocess.py
    security fix

--- mlops/sagemaker-pipelines/sagemaker-pipelines-sample/postprocess/postprocess.py
+++ mlops/sagemaker-pipelines/sagemaker-pipelines-sample/postprocess/postprocess.py
@@ -18,7 +18,27 @@ def arg_parse():
 def predict(input_model_dir:str, test_x):
     model_path = os.path.join(input_model_dir, 'model.tar.gz')
     with tarfile.open(model_path) as tar:
-        tar.extractall(path=".")
+        
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path=".")
     model = pickle.load(open("xgboost-model", "rb"))
     pred_y = model.predict(test_x)
     return pred_y
