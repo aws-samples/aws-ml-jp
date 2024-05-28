859d04bf8bd1eda914f1fd6b14080136de349f0e
Kazuki Motohashi <kazmot@amazon.co.jp>
2024-05-29 00:54:40 +0900

    add requirements.txt and remove gabages

--- tasks/generative-ai/advanced-rag/app/templates/comparison_view.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def comparison_view(rag_result, advanced_rag_result):
-    st.markdown("### Comparison View")
-    st.write("#### RAG Result")
-    st.write(rag_result)
-    st.write("#### Advanced RAG Result")
-    st.write(advanced_rag_result)
--- tasks/generative-ai/advanced-rag/app/templates/detail_view.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-def detail_view(item):
-    st.markdown("### Detail View")
-    st.write("Title:", item["Title"])
-    st.write("Content:", item["Content"])
-    st.write("Score:", item["Score"])
